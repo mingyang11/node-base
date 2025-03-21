33017367dd0e90a6c524aa3b2f6c939de1c01f6d
mingyang11 <2578349261@qq.com>
2025-03-21 12:00:10 +0800

    fix

---
+++ utils.js
@@ -0,0 +1,24 @@
+/**
+ * 一个简单的加法函数
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * 一个简单的减法函数
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  add,
+  subtract,
+};
