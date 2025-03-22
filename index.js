e409b36717633f12905c50f5f01f9187022f4a73
mingyang11 <2578349261@qq.com>
2025-03-22 08:51:26 +0800

    feat: add os module

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const path = require('node:path');
+const { exec } = require('node:child_process');
+const os = require('node:os');
 
-// path.basename: 返回路径的目录名
-console.log(path.basename('/User'), 'basename');
+// plateform: 获取操作平台
+console.log(os.platform());
 
-// path.extname: 返回文件拓展名
-console.log(
-  path.extname('/Users/yangming/Desktop/projectOwn/node-base/index.js')
-);
+// release: 获取操作系统版本号
+console.log(os.release());
 
-// path.join: 拼接路径
-console.log(
-  path.join('/Users', 'yangming', 'Desktop/projectOwn/node-base', 'index.js')
-);
+// version： 操作系统版本
+console.log(os.version());
 
-// path.resolve: 解析路径, 返回的是绝对路径
-// 都是绝对路径返回最后一个
-// 如果只有一个相对路径，返回当前工作目录的绝对路径
-console.log(path.resolve('/Users/yangming'));
-console.log(path.resolve('/Users/yangming', 'fuyao.js'));
-console.log(__dirname, '__dirname');
-console.log(__filename, '__filename');
+const open = (url) => {
+  const plateform = os.platform();
+  if (plateform === 'darwin') {
+    exec(`open ${url}`);
+  } else if (plateform === 'win32') {
+    exec(`start ${url}`);
+  } else if (plateform === 'linux') {
+    exec(`xdg-open ${url}`);
+  }
+};
 
-// path.parse: 解析路径，返回一个对象path.format()逆向操作
-console.log(
-  path.parse('/Users/yangming/Desktop/projectOwn/node-base/index.js')
-);
-// path.format: path.parse的逆向操作
-console.log(
-  path.format({
-    root: '/',
-    dir: '/Users/yangming/Desktop/projectOwn/node-base',
-    base: 'index.js',
-    ext: '.js',
-    name: 'index',
-  })
-);
+// open('http://www.baidu.com');
 
-// path.sep: 如果四window返回 \, 如果是mac返回 / 。为了实现跨平台的路径符号统一
-console.log(path.sep);
+// homedir: 读取当前目录
+console.log(os.homedir());
+
+// arch: cpu架构
+console.log(os.arch());
+
+// cpus: 操作系统线程cpu信息
+// console.log(os.cpus());
+
+// 获取网络信息
+console.log(os.networkInterfaces());
