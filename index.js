5054380e559712c8617bd19fbd56d96c341b2ce1
mingyang11 <2578349261@qq.com>
2025-03-22 14:23:08 +0800

    feat: add util

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const { execSync } = require('child_process');
+import util from 'node:util';
+import { exec } from 'node:child_process';
 
-execSync('open "/Applications/抖音.app"');
+// promisify：将回调函数变为promise函数
+const execPromise = util.promisify(exec);
+// 将promise转为promise
+const execCallbackify = util.callbackify();
+
+execPromise('node -v').then(({ stdout, stderr }) => {
+  console.log(stdout);
+});
+
+// 转换匹配占位符
+util.format('%s---%s', 'chuze', 'fuyao');
