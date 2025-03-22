203387b58e7b9593bd001a40c6fb5cf6df88946d
mingyang11 <2578349261@qq.com>
2025-03-22 12:46:51 +0800

    feat: add utils

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// 基于发布订阅模式的
-const eventEmiter = require('node:events');
+const { execSync } = require('child_process');
 
-const bus = new eventEmiter();
-// 默认只能监听10，这个可以修改监听事件数量
-bus.setMaxListeners(20);
-
-// 获取最大监听数量
-console.log(bus.getMaxListeners());
-
-const fn = (msg) => {
-  console.log(msg);
-};
-
-bus.on('test', fn);
-// off需要判断事件名和事件处理函数都一致的情况下才会关掉
-bus.off('test', fn);
-
-// 只会触发一次
-bus.emit('test', 'wqnmd');
-bus.emit('test', 'wqnmd');
+execSync('open "/Applications/抖音.app"');
